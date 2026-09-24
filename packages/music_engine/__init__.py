8fb9d47588d935bfbe3012dcdf24c75485817b81
gnefolo <84088116+gnefolo@users.noreply.github.com>
2026-09-24 15:30:22 +0200

    chore: export tuning intelligence API

--- packages/music_engine/__init__.py
+++ packages/music_engine/__init__.py
@@ -15,6 +15,7 @@ from .engine import (
 from .intelligence import analyze_chord, analyze_guitar_intelligence, analyze_progression, assign_fingers, detect_barres
 from .corrections import CorrectionRecord, FingeringAnchor, build_correction_record, optimize_with_anchors, validate_anchor
 from .learned_ranker import LearnedRankerModel, optimize_polyphonic_fingering_learned, train_pairwise_ranker
+from .tuning_intelligence import TuningCompatibility, score_tuning_compatibility, suggest_tunings
 from .rhythm import RhythmConfig, TimeSignature, QuantizedTabNote, quantize_tab_notes
 from .musicxml import export_musicxml
 
@@ -26,6 +27,7 @@ __all__ = [
     "assign_fingers", "detect_barres",
     "CorrectionRecord", "FingeringAnchor", "build_correction_record", "optimize_with_anchors", "validate_anchor",
     "LearnedRankerModel", "optimize_polyphonic_fingering_learned", "train_pairwise_ranker",
+    "TuningCompatibility", "score_tuning_compatibility", "suggest_tunings",
     "RhythmConfig", "TimeSignature", "QuantizedTabNote", "quantize_tab_notes",
     "export_musicxml",
 ]
